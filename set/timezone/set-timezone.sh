3c4359a0ba0409333ca14a88ec296d8a4cf43f26
Tomasz <tomczak111@gmail.com>
2022-12-07 22:15:29 +0100

    added new function - font colors

--- set/timezone/set-timezone.sh
+++ set/timezone/set-timezone.sh
@@ -2,15 +2,18 @@
 
 set -e
 
+source_dir="$(dirname "$0")"
+source ${source_dir}/../../source/font.sh
+
 TIMEZONE="${1}"
 
-echo "Set timezone - ${TIMEZONE}"
+echo-info "Setting timezone - ${TIMEZONE}"
 
 if [ -f /usr/share/zoneinfo/"${TIMEZONE}" ]; then
     ln -snf /usr/share/zoneinfo/"${TIMEZONE}" /etc/localtime
     echo "${TIMEZONE}" > /etc/timezone
     date
 else
-    echo "Bad time zone!"
+    echo-error "Bad time zone!"
     exit 1
 fi
--- source/font.sh
+++ source/font.sh
@@ -3,18 +3,18 @@
 set -e
 
 
-function echo-error{
+function echo-error {
     echo -e "\033[1;31m $1 \033[00m"
 }
 
-function echo-info{
+function echo-info {
     echo -e "\033[1;33m $1 \033[00m"
 }
 
-function echo-success{
+function echo-success {
     echo -e "\033[1;32m $1 \033[00m"
 }
 
-function echo-normal{
-    echo -e "\033[0m $"1
+function echo-normal {
+    echo -e "\033[0m $1"
 }

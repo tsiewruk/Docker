16aefe352e82ead562301087a1f5a839247fb3c4
Tomasz <tomczak111@gmail.com>
2022-12-15 22:10:00 +0100

    added script aliases

--- set/alias/set-alias.sh
+++ set/alias/set-alias.sh
@@ -8,10 +8,10 @@ source ${source_dir}/../../source/font.sh
 echo-info "Setting aliases"
 
 if [ -f ~/.bash_aliases ]; then
-. ~/.bash_aliases
+    . ~/.bash_aliases
 fi
 
-echo "alias 'service status'='bash /etc/status.sh'"
-echo "alias 'service restart'='bash /etc/restart.sh'"
-echo "alias 'service stop'='bash /etc/stop.sh'"
-echo "alias 'service start'='bash /etc/start.sh'"
+echo "alias 'service status'='bash /etc/status.sh'" >> ~/.bash_aliases
+echo "alias 'service restart'='bash /etc/restart.sh'" >> ~/.bash_aliases
+echo "alias 'service stop'='bash /etc/stop.sh'" >> ~/.bash_aliases
+echo "alias 'service start'='bash /etc/start.sh'" >> ~/.bash_aliases

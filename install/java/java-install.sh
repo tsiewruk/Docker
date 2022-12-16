79895024f7da178f65de8c972d234fcc49d86787
Tomasz <tomczak111@gmail.com>
2022-12-16 20:13:42 +0100

    refactor #!/bin/bash

--- install/java/java-install.sh
+++ install/java/java-install.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
 source_dir="$(dirname "$0")"
 source ${source_dir}/../../source/font.sh
+
+cp -R "${source_dir/files/service}" /
 
 JAVA_VERSION="$1"
 
@@ -11,4 +13,3 @@ echo-info "Installing Java-${JAVA_VERSION}"
 
 apt-get update
 apt-get install -y "openjdk-${JAVA_VERSION}-jre"
-
--- install/nginx/nginx-install.sh
+++ install/nginx/nginx-install.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-source_dir="$(dirname "$0")"
+source_dir="$(dirname $(readlink -f "$0"))"
 source ${source_dir}/../../source/font.sh
 
+cp -R "${source_dir/files/service}" /
 
 echo-info "Installing NGINX"
 

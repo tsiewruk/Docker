a202e8c6832587676ac9b9e487626f7e364f4534
Tomasz <tomczak111@gmail.com>
2023-01-01 21:35:57 +0100

    added varnish builder

---
+++ install/varnish/varnish-install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+source_dir="$(dirname $(readlink -f "$0"))"
+source ${source_dir}/../../source/font.sh
+
+cp -R "${source_dir/files/service}" /
+
+echo-info "Installing Varnish"
+apt-get -y update
+apt-get -y install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" varnish

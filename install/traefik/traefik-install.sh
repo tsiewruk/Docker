3b8d68572d6b9c93c28000f3a16e505ca182e92f
Tomasz <tomczak111@gmail.com>
2023-01-02 19:50:43 +0100

    changed traefik install script

--- install/traefik/traefik-install.sh
+++ install/traefik/traefik-install.sh
@@ -3,7 +3,7 @@
 set -e
 
 source_dir="$(dirname $(readlink -f "$0"))"
-source ${source_dir}/../../../source/font.sh
+source ${source_dir}/../../source/font.sh
 
 cp -R "${source_dir/files/service}" /
 
@@ -11,7 +11,7 @@ TRAEFIK_VERSION="${1}"
 
 echo-info "Installing Traefik"
 
-wget "https://github.com/traefik/traefik/releases/download/v2.9.6/traefik_v${TRAEFIK_VERSION}_linux_amd64.tar.gz" -P /tmp/traefik
+wget "https://github.com/traefik/traefik/releases/download/v${TRAEFIK_VERSION}/traefik_v${TRAEFIK_VERSION}_linux_amd64.tar.gz" -P /tmp/traefik
 tar -xf "/tmp/traefik/traefik_v${TRAEFIK_VERSION}_linux_amd64.tar.gz" -C /usr/local/bin/ traefik
 
 rm -rf /tmp/traefik

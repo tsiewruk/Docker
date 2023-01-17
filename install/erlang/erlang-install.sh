0437df8d8586fa4f5062077d78b82faa58a28116
Tomasz <tomczak111@gmail.com>
2023-01-17 21:25:09 +0100

    added rabbit&erlang installers

---
+++ install/erlang/erlang-install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+source_dir="$(dirname $(readlink -f "$0"))"
+source ${source_dir}/../../source/font.sh
+
+ERLANG_VERISON="${1}"
+UBUNTU_RELEASE="${2}"
+
+echo-info "Installing Erlang"
+
+## Launchpad PPA that provides modern Erlang releases
+curl -1sLf "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xf77f1eda57ebb1cc" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/net.launchpad.ppa.rabbitmq.erlang.gpg > /dev/null
+
+## Add apt repositories maintained by Team RabbitMQ
+sudo tee /etc/apt/sources.list.d/rabbitmq.list <<EOF
+## Provides modern Erlang/OTP releases
+deb [signed-by=/usr/share/keyrings/net.launchpad.ppa.rabbitmq.erlang.gpg] http://ppa.launchpad.net/rabbitmq/rabbitmq-erlang/ubuntu "${UBUNTU_RELEASE}" main
+deb-src [signed-by=/usr/share/keyrings/net.launchpad.ppa.rabbitmq.erlang.gpg] http://ppa.launchpad.net/rabbitmq/rabbitmq-erlang/ubuntu "${UBUNTU_RELEASE}" main
+EOF
+
+## Update package indices
+apt-get update -y
+
+## Install Erlang packages
+apt-get install -y erlang-base \
+                        erlang-asn1 erlang-crypto erlang-eldap erlang-ftp erlang-inets \
+                        erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key \
+                        erlang-runtime-tools erlang-snmp erlang-ssl \
+                        erlang-syntax-tools erlang-tftp erlang-tools erlang-xmerl

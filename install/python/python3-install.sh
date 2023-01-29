077748a3068bb5b469e87041ca6adf324f8d2bd4
Tomasz <tomczak111@gmail.com>
2023-01-29 19:01:37 +0100

    added python install script

--- install/python/python3-install.sh
+++ install/python/python3-install.sh
@@ -3,14 +3,17 @@
 set -e
 
 source_dir="$(dirname $(readlink -f "$0"))"
-source "${source_dir}/../../../source/font.sh"
+source "${source_dir}/../../source/font.sh"
 
-PYTHON_VERSION="{1}"
+PYTHON_VERSION="${1}"
 
 echo-info "Installing Python"
 
+apt-get update -y
+apt-get install -y --no-install-recommends software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update -y
-apt-get install -y --no-install-recommends "python${PYTHON_VERSION}" "python${PYTHON_VERSION}-distutils" "python${PYTHON_VERSION}-pip"
+apt-get install -y "python${PYTHON_VERSION}" "python${PYTHON_VERSION}-distutils" "python$(echo ${PYTHON_VERSION} | awk -F. '{print $1}')-pip"
+python3 -m pip install --upgrade pip
 
 update-alternatives --install /usr/bin/python3 python3 "/usr/bin/python${PYTHON_VERSION}" 1

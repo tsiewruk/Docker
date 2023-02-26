029fb57c3b93f7f7a65d3376fabd4f6fa81ba2af
Tomasz Siewruk <tomasz.m.siewruk@gmail.com>
2023-02-26 20:31:29 +0100

    added mysql builder

--- build/mysql/files/usr/local/bin/entrypoint.sh
+++ build/mysql/files/usr/local/bin/entrypoint.sh
@@ -2,22 +2,4 @@
 
 set -e
 
-if [ ! -f "/etc/scripts.env" ]; then
-    touch /etc/scripts.env
-    echo "export a_=1" >> /etc/scripts.env
-fi
-
-source /etc/scripts.env
-
-env_array=( $(env | sort) )
-entrypoint_scripts_path='/usr/local/bin/entrypoint-scripts/'
-
-for var in "${env_array[@]}"; do
-    if [ "${var: -1}" = "1" ]; then
-        env_name=$(echo ${var:0:-2})
-        [ -x "${entrypoint_scripts_path}${env_name}.sh" ] && "${entrypoint_scripts_path}${env_name}.sh"
-    fi
-done
-
-source /etc/scripts.env
 exec s6-svscan -t0 /service

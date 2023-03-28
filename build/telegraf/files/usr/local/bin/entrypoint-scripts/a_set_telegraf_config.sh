30d332c45edc1515b675ce5febb9b745e6f59a40
Tomasz Siewruk <tomasz.m.siewruk@gmail.com>
2023-03-28 20:46:04 +0200

    added telegraf to docker-compose

--- build/telegraf/files/usr/local/bin/entrypoint-scripts/a_set_telegraf_config.sh
+++ build/telegraf/files/usr/local/bin/entrypoint-scripts/a_set_telegraf_config.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+INFLUXDB_URL="${1}"
+INFLUXDB_USERNAME="${2}"
+INFLUXDB_PASSWORD="${3}"
+
 telegraf_config_path=/etc/telegraf.conf
 sed -i "s|<INFLUXDB_URL>|$INFLUXDB_URL|g" $telegraf_config_path
 sed -i "s|<INFLUXDB_USERNAME>|$INFLUXDB_USERNAME|g" $telegraf_config_path

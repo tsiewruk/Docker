01f6c3e9ff30f0bad0de3989d9bb8b06a23601ab
Tomasz Siewruk <tomasz.m.siewruk@gmail.com>
2023-03-28 20:13:57 +0200

    added telegraf builder

---
+++ build/terraform/files/usr/local/bin/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -f "/etc/scripts.env" ]; then
+    touch /etc/scripts.env
+    echo "export a_set_telegraf_config=1" >> /etc/scripts.env
+fi
+source /etc/scripts.env
+
+env_array=( $(env | sort) )
+entrypoint_scripts_path='/usr/local/bin/entrypoint-scripts/'
+
+for var in "${env_array[@]}"; do
+    if [ "${var: -1}" = "1" ]; then
+        env_name=$(echo ${var:0:-2})
+        [ -x "${entrypoint_scripts_path}${env_name}.sh" ] && "${entrypoint_scripts_path}${env_name}.sh"
+    fi
+done
+source /etc/scripts.env
+
+exec s6-svscan -t0 /service

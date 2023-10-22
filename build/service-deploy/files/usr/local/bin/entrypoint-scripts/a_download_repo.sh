8a995431413b569c92559d0915bc0ff23514290b
Tomasz <user@tomaszowy.fritz.box>
2023-10-22 20:27:53 +0200

    refactor

---
+++ build/service-deploy/files/usr/local/bin/entrypoint-scripts/a_download_repo.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "todo"
+
+sed -i "s|a_download_repo=1|a_download_repo=0|" /etc/scripts.env
--- build/service-deploy/files/usr/local/bin/entrypoint-scripts/a_set_config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-rabbitmqctl import_definitions "/path/to/definitions.file.json"
-
-sed -i "s|a_set_config=1|a_set_config=0|" /etc/scripts.env

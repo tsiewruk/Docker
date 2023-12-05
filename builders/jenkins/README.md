10931b0e7e9c96538b1499ff1187a610633aecac
Tomasz <user@tomaszowy.fritz.box>
2023-12-05 21:11:23 +0100

    feat: add jenkins install script

--- builders/jenkins/README.md
+++ builders/jenkins/README.md
@@ -3,4 +3,7 @@
 1. Create volume
 `docker volume create jenkins-volume`
 2. Start container
-`docker compose up -d traefik jenkins`
+`docker compose up -d jenkins`
+1. Check Jenkins temp admin password.
+`docker container logs jenkins`
+1. Go to website localhost:8080 configure the service
--- cookbooks/installers/jenkins/jenkins-install.sh
+++ cookbooks/installers/jenkins/jenkins-install.sh
@@ -4,13 +4,17 @@ set -e
 
 . "${TMP_COOKBOOKS_DIR}/source/font.sh"
 
+source_dir=$(dirname $(readlink -f "$0"))
+cp -R "${source_dir}/files/service" /
+
 JENKINS_VERSION="${1}"
 
 ####################################
 echo-info "Installing Jenkins version - ${JENKINS_VERSION}"
 ####################################
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 apt-get update -y
 apt-get install -y jenkins

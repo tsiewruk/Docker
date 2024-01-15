8c9a5472ee4e88db64eb7bb5c5fabba19fbc2a5b
Tomasz Siewruk <tomasz.m.siewruk@gmail.com>
2024-01-15 19:52:22 +0100

    add: wazuh

--- cookbooks/installers/php/composer/composer-install.sh
+++ cookbooks/installers/php/composer/composer-install.sh
@@ -7,7 +7,7 @@ set -e
 PHP_COMPOSER_VERSION="${1}"
 
 ####################################
-echo-info "Installing Composer"
+echo-info "Installing Composer - ${PHP_COMPOSER_VERSION}"
 ####################################
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --version="${PHP_COMPOSER_VERSION}"

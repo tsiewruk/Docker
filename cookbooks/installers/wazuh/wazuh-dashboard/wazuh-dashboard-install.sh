085211473787456e27d3e42e52c0b1517f3e9fce
Tomasz <user@tomaszowy.fritz.box>
2024-03-24 20:49:23 +0100

    update: readme

---
+++ cookbooks/installers/wazuh/wazuh-dashboard/wazuh-dashboard-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+. "${TMP_COOKBOOKS_DIR}/source/font.sh"
+
+#source_dir=$(dirname $(readlink -f "$0"))
+#cp -R "${source_dir}/files/service" /
+
+####################################
+echo-info "Installing Wazuh Dashboard - ${WAZUH_VERSION}"
+####################################
+
+# requirements
+apt-get update -y
+apt-get install -y --no-install-recommends debhelper libcap2-bin
+
+# add repositry
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
+echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+apt-get update -y
+
+# install wazuh-dashboard
+apt-get -y install --no-install-recommends wazuh-dashboard

f7ebbf72b4e47b6cd30d8a4f21f65a78e5329427
Tomasz Siewruk <tomasz.m.siewruk@gmail.com>
2024-04-16 17:57:48 +0200

    add: open-tofu installation script

--- cookbooks/installers/open-tofu/open-tofu-install.sh
+++ cookbooks/installers/open-tofu/open-tofu-install.sh
@@ -10,14 +10,7 @@ OPEN_TOFU_VERISON="${1}"
 echo-info "Installing Open TOFU version - ${OPEN_TOFU_VERISON}"
 ####################################
 
-curl -fsSL https://get.opentofu.org/opentofu.gpg | sudo tee /etc/apt/keyrings/opentofu.gpg >/dev/null
-curl -fsSL https://packages.opentofu.org/opentofu/tofu/gpgkey | sudo gpg --no-tty --batch --dearmor -o /etc/apt/keyrings/opentofu-repo.gpg >/dev/null
-sudo chmod a+r /etc/apt/keyrings/opentofu.gpg
+wget "https://github.com/opentofu/opentofu/releases/download/v${OPEN_TOFU_VERISON}/tofu_${OPEN_TOFU_VERISON}_amd64.deb" -P /tmp/open-tofu
+dpkg -i /tmp/open-tofu/tofu_${OPEN_TOFU_VERISON}_amd64.deb
 
-echo \
-  "deb [signed-by=/etc/apt/keyrings/opentofu.gpg,/etc/apt/keyrings/opentofu-repo.gpg] https://packages.opentofu.org/opentofu/tofu/any/ any main
-deb-src [signed-by=/etc/apt/keyrings/opentofu.gpg,/etc/apt/keyrings/opentofu-repo.gpg] https://packages.opentofu.org/opentofu/tofu/any/ any main" | \
-  sudo tee /etc/apt/sources.list.d/opentofu.list > /dev/null
-
-apt-get -y update
-apt-get -y install --no-install-recommends tofu
+rm -rf /tmp/open-tofu

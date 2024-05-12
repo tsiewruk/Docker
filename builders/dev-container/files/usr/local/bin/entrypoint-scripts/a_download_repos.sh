0aac87300c5e7d334faab3610e96a3b246d7a08c
tomasz <tomasz.m.siewruk@gmail.com>
2024-05-12 19:55:38 +0200

    fix: entrypoint script

--- builders/dev-container/files/usr/local/bin/entrypoint-scripts/a_download_repos.sh
+++ builders/dev-container/files/usr/local/bin/entrypoint-scripts/a_download_repos.sh
@@ -4,9 +4,10 @@ set -e
 
 if [ ! -d "/opt/ansible" ]; then
     git clone https://github.com/tsiewruk/Ansible.git
+fi
 
 if [ ! -d "/opt/terraform" ]; then
     git clone https://github.com/tsiewruk/Terraform.git
 fi
 
-sed -i "s|a_download_repos=1|a_download_repos=0|" /etc/entrypoint-scripts.env
+sed -i "s|a_download_repos=1|a_download_repos=0|" /usr/local/bin/entrypoint-scripts.env

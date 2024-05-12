2b8046514349138c856081eb8b9866a1316a725a
tomasz <tomasz.m.siewruk@gmail.com>
2024-05-12 20:18:52 +0200

    fix: entry-point values

--- builders/dev-container/files/usr/local/bin/entrypoint-scripts/a_download_repos.sh
+++ builders/dev-container/files/usr/local/bin/entrypoint-scripts/a_download_repos.sh
@@ -2,12 +2,13 @@
 
 set -e
 
+repository_path="/opt"
 if [ ! -d "/opt/ansible" ]; then
-    git clone https://github.com/tsiewruk/Ansible.git
+    git clone https://github.com/tsiewruk/Ansible.git ${repository_path)/ansible
 fi
 
 if [ ! -d "/opt/terraform" ]; then
-    git clone https://github.com/tsiewruk/Terraform.git
+    git clone https://github.com/tsiewruk/Terraform.git ${repository_path)/terraform
 fi
 
 sed -i "s|a_download_repos=1|a_download_repos=0|" /usr/local/bin/entrypoint-scripts.env
